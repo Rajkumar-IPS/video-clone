57b96b8b20499c5f3f68a1b66c9ec3be1d4eb75b
vivek-IPS <vivekr.itpath@gmail.com>
2022-07-28 18:51:50 +0530

    pull

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,97 +1,107 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { Player, ControlBar, ReplayControl, ForwardControl, BigPlayButton, ProgressControl, VolumeMenuButton, PlayToggle, FullscreenToggle } from 'video-react';
 import "../../node_modules/video-react/dist/video-react.css";
 import LockIcon from './lockIcon';
-
+import movies from '../movie.json'
 
 const VideoPlayer = () => {
     const location = useLocation();
-    const [lockStatus, setLockStatus] = useState(true)
-    const [player, setPlayer] = useState(true)
-    const [currentTime, setCurrentTime] = useState()
+    const [ lockStatus, setLockStatus ] = useState( true )
+    const [ player, setPlayer ] = useState( true )
+    // const [ currentTime, setCurrentTime ] = useState()
 
-
+    const { id, currentTime } = useParams()
 
     const lockScreenFun = () => {
         lockStatus == true ?
-            setLockStatus(false) :
-            setLockStatus(true)
+            setLockStatus( false ) :
+            setLockStatus( true )
     }
 
     const leftDoubleClick = () => {
-        console.log("left");
-        if (lockStatus == true) {
-            player.replay(10)
+        console.log( "left" );
+        if ( lockStatus == true )
+        {
+            player.replay( 10 )
         }
     }
 
     const rightDoubleClick = () => {
-        console.log("right");
-        if (lockStatus == true) {
-            player.forward(10)
+        console.log( "right" );
+        if ( lockStatus == true )
+        {
+            player.forward( 10 )
         }
 
     }
 
+    const filterMovies = movies.videos.filter( val => val.id === id )
+    console.log( 'filterMovies', filterMovies )
 
 
     return (
-
         <>
-
-            <div>{location.state.src}</div>
-            <div>{location.state.currentTime}</div>
-
-
-            <div className='d-flex justify-content-center'>
-                <Player
-                    fluid={false}
-                    poster="/assets/poster.png"
-                    src={location.state.src}
-                    preload='none'
-                    className="hoverrrr"
-                    width={900}
-                    height={600}
-                    ref={player => {
-                        setPlayer(player)
-                    }}
-                    startTime={location.state.currentTime}
-                    
-                    autoPlay={true}
+            {
+                filterMovies.map( val => <>
+                    {/* 
+                    <div>{ val.sources }</div>
+                    <div>{ currentTime }</div> */}
 
 
-                >
-                    <div className='d-flex' style={{ justifyContent: "space-between", height: "92%" }} >
-                        <div className='' style={{ zIndex: 999, cursor: "pointer", width: "25%" }} onDoubleClick={leftDoubleClick}></div>
-                        <div className='' style={{ zIndex: 999, cursor: "pointer", width: "25%" }} onClick={rightDoubleClick}></div>
-                    </div>
-                    <BigPlayButton position="center" />
-                    {
-                        lockStatus == true ?
-                            <ControlBar autoHide={true} autoHideTime={3000} disableDefaultControls>
-                                {/* <p id='lock' style={{ fontSize: "14px" }} onClick={() => setLockStatus(false)} >lock</p> */}
-                                <LockIcon lockScreenFun={lockScreenFun} lockStatus={lockStatus} />
+                    <div className='d-flex justify-content-center align-items-center main-section'>
+                        <div className='video-player-box'>
+                            <Player
+                                fluid={ true }
+                                poster="/assets/poster.png"
+                                src={ val.sources }
+                                preload='none'
+                                className="hoverrrr"
+                                // width={ 900 }
+                                // height={ 600 }
+                                ref={ player => {
+                                    setPlayer( player )
+                                } }
+                                startTime={ currentTime ? currentTime : 0 }
+                                muted={ true }
+                                autoPlay={ true }
 
-                                <PlayToggle order={1} />
-                                <ReplayControl seconds={10} order={2.1} />
-                                <ForwardControl seconds={10} order={2.2} />
-                                <VolumeMenuButton order={1} vertical={true} />
-                                <ProgressControl width={1} />
-                                <FullscreenToggle className="ms-auto" order={3.1} />
 
-                            </ControlBar> :
-                            <ControlBar disableDefaultControls={true} >
-                                {/* <p id='lock' style={{ fontSize: "14px" }} onClick={() => setLockStatus(true)} >unlock</p> */}
-                                <LockIcon lockScreenFun={lockScreenFun} />
+                            >
+                                <div className='d-flex' style={ { justifyContent: "space-between", height: "92%" } } >
+                                    <div className='' style={ { zIndex: 999, cursor: "pointer", width: "25%" } } onDoubleClick={ leftDoubleClick }></div>
+                                    <div className='' style={ { zIndex: 999, cursor: "pointer", width: "25%" } } onClick={ rightDoubleClick }></div>
+                                </div>
+                                <BigPlayButton position="center" />
+                                {
+                                    lockStatus == true ?
+                                        <ControlBar autoHide={ false } autoHideTime={ 3000 } disableDefaultControls>
+                                            {/* <p id='lock' style={{ fontSize: "14px" }} onClick={() => setLockStatus(false)} >lock</p> */ }
+                                            <LockIcon lockScreenFun={ lockScreenFun } lockStatus={ lockStatus } />
 
-                            </ControlBar>
-                    }
+                                            <PlayToggle order={ 1 } />
+                                            <ReplayControl seconds={ 10 } order={ 2.1 } />
+                                            <ForwardControl seconds={ 10 } order={ 2.2 } />
+                                            <VolumeMenuButton order={ 1 } vertical={ true } />
+                                            <ProgressControl width={ 1 } />
+                                            <FullscreenToggle className="ms-auto" order={ 3.1 } />
 
-                </Player >
-            </div >
+                                        </ControlBar> :
+                                        <ControlBar disableDefaultControls={ true } >
+                                            {/* <p id='lock' style={{ fontSize: "14px" }} onClick={() => setLockStatus(true)} >unlock</p> */ }
+                                            <LockIcon lockScreenFun={ lockScreenFun } />
+
+                                        </ControlBar>
+                                }
+
+                            </Player >
+                        </div>
+                    </div >
+                </> )
+            }
         </>
     )
+
 }
 
 export default VideoPlayer
--- src/components/lockIcon.js
+++ src/components/lockIcon.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react'
 import lock from "../icons8-lock-90.png"
 import unlock from "../icons8-unlock-30.png"
 
-const LockIcon = (props) => {
-    const [lockStatus, setLockStatus] = useState(true)
+const LockIcon = ( props ) => {
+    const [ lockStatus, setLockStatus ] = useState( true )
 
     return (
-        <>
-            {props.lockStatus == true ?
+        <><div className='lock-div'>
+            { props.lockStatus == true ?
 
-                <img src={lock} style={{ cursor: "pointer", width: "25px", height: "25px" }} onClick={props.lockScreenFun} />
-                : <img src={unlock} style={{ cursor: "pointer", width: "25px", height: "25px" }} onClick={props.lockScreenFun} />}
+                <img src={ lock } className="lock-icon" onClick={ props.lockScreenFun } />
+                : <img src={ unlock } className="lock-icon" onClick={ props.lockScreenFun } /> }
+        </div>
         </>
     )
 }
